807caca096b0cbbcef8e7d838515aa96d4c2272c
Sunnatillo <sunnatilloshavkatov@gmail.com>
2026-03-02 17:49:53 +0500

    refactor: remove CallKit implementation and retain stub for compatibility

--- ios/Classes/PictureInPicture/SDKCallKitManager.h
+++ ios/Classes/PictureInPicture/SDKCallKitManager.h
@@ -1,25 +1,16 @@
 //
-//  MobileRTCCallKitManager.h
-//  MobileRTCSample
+//  SDKCallKitManager.h
 //
-//  Created by Zoom on 2023/7/10.
-//  Copyright © 2023 Zoom Video Communications, Inc. All rights reserved.
+//  CallKit removed. Stub interface kept for source compatibility.
 //
 
-#import <CallKit/CallKit.h>
 #import <Foundation/Foundation.h>
-#import <ZoomVideoSDK/ZoomVideoSDK.h>
 
-// Apple MIIT requirement: CallKit must be disabled in China App Store.
-// Set to 1 to enable CallKit (disabled automatically for Chinese locales).
-#define ENABLE_CALLKIT_VOIP_MEETING 1
-
-@interface SDKCallKitManager : NSObject <CXProviderDelegate>
+@interface SDKCallKitManager : NSObject
 
 @property(nonatomic, assign, readonly) BOOL isInCall;
 
 + (instancetype)sharedManager;
-+ (BOOL)isChineseRegion;
 - (void)startCallWithHandle:(NSString *)handle
                    complete:(void (^)(void))completion;
 - (void)endCallWithComplete:(void (^)(void))completion;
